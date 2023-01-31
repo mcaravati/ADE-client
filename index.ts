d69b3e89c5f8bea39394bb80a8c9c9433f3cd917
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-01-31 10:45:30 +0100

    Added a method to get the planning for a room

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {IADEClient} from "./IADEClient";
+import type {IADEClient, Room} from "./IADEClient";
 import {createClient} from "./ADEClient";
 
 (async () => {
@@ -9,6 +9,10 @@ import {createClient} from "./ADEClient";
     await client.sendConnectionRequest();
     await client.initProject();
 
-    const uid = '';
+    const uid = 'skhalifa';
     console.log(await client.getADEId(uid));
+    let r: Room = {id: "3272", name: "EA-I111/I113", isFolder: false, parent: null}
+    await client.getPlanningForRoom(r, new Date(), new Date())
+    console.log(r.edt)
+
 })();
